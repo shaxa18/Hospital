7abfff846cbb50b1809663e2af2b0191cfdde3f5
imomo <imomovshoh21@gmail.com>
2022-06-27 22:12:13 +0500

    hospital project created

---
+++ src/main/java/com/napa/Kasalxona/KasalxonaApplication.java
@@ -0,0 +1,13 @@
+package com.napa.Kasalxona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasalxonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasalxonaApplication.class, args);
+	}
+
+}
